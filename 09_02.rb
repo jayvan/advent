e5aa76a7820709bada6af19d011d2ffa262f5325
Justin Vanderheide <justin@justinvanderheide.com>
2015-12-09 11:25:19 -0500

    Day 9: All in a Single Night

---
+++ 09_02.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/env ruby
+
+def search(graph, current = nil, visited = [])
+  targets = graph.keys - visited - [nil]
+  return 0 if targets.empty?
+
+  targets.map do |target|
+    graph[current][target] + search(graph, target, visited + [target])
+  end.max
+end
+
+graph = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+ARGF.each_line do |line|
+  a, _, b, _, cost = line.chomp.split(' ')
+
+  graph[a][b] = cost.to_i
+  graph[b][a] = cost.to_i
+end
+puts search(graph)
