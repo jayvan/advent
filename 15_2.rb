a936a7d14e02e3334a66d34b3d2769fcb8e0d47c
Justin Vanderheide <justin@justinvanderheide.com>
2015-12-17 08:23:23 -0500

    Day 15: Science for Hungry People

---
+++ 15_2.rb
@@ -0,0 +1,31 @@
+#! /usr/bin/env ruby
+AMOUNT = 100
+
+ingredients = []
+ARGF.each_line do |line|
+ data = line.chomp.split(' ').map(&:to_i)
+ ingredients << [data[2], data[4], data[6], data[8], data[10]]
+end
+
+def score(ingredients, amounts)
+  scores = ingredients.each_with_index.map do |ingredient, i|
+    ingredient.map { |quality| quality * amounts[i] }
+  end
+
+  sums = scores.transpose.map {|x| x.inject(:+)}
+  return 0 unless sums.last == 500
+  return 0 if sums[0..-2].any? { |x| x <= 0 }
+  sums[0..-2].inject(:*)
+end
+
+def search(ingredients, ingredient = 0, remaining = AMOUNT, amounts = [])
+  if ingredient == ingredients.length - 1
+    return score(ingredients, amounts + [remaining])
+  end
+
+  0.upto(remaining).map do |i|
+    search(ingredients, ingredient + 1, remaining - i, amounts + [i])
+  end.max
+end
+
+puts search(ingredients)
