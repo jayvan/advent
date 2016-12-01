512c5a08bb986eb0a2d103619834bd43ad836f95
Justin Vanderheide <justin@justinvanderheide.com>
2016-12-01 00:51:27 -0500

    Day 1: No Time for a Taxicab

---
+++ 2016/01.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Advent01 {
+  enum Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+  }
+  public static void Main () {
+    string[] commands = Console.ReadLine().Split(new [] {", "}, StringSplitOptions.None);
+    Direction direction = Direction.UP;
+    var visited = new HashSet<Tuple<int, int>>();
+    visited.Add(new Tuple<int, int>(0, 0));
+    bool doubledUp = false;
+    int x = 0;
+    int y = 0;
+
+    foreach (string command in commands) {
+      if (command[0] == 'R') {
+        direction = (Direction)(((int)direction + 1) % 4);
+      } else {
+        direction = (Direction)(((int)direction + 3) % 4);
+      }
+
+      int distance = int.Parse(command.Substring(1));
+
+      for (int j = 0; j < distance; j++) {
+        switch (direction) {
+          case Direction.UP:
+            y++;
+            break;
+          case Direction.DOWN:
+            y--;
+            break;
+          case Direction.LEFT:
+            x--;
+            break;
+          case Direction.RIGHT:
+            x++;
+            break;
+        }
+
+        if (!doubledUp) {
+          var location = new Tuple<int, int>(x, y);
+
+          if (visited.Contains(location)) {
+            Console.WriteLine("First Duplicate Distance: " + (Math.Abs(x) + Math.Abs(y)));
+            doubledUp = true;
+          } else {
+            visited.Add(location);
+          }
+        }
+      }
+    }
+
+    Console.WriteLine("Distance: " + (Math.Abs(x) + Math.Abs(y)));
+  }
+}
