e4e5ff10ae5b9df2f1613bde495278ef811897e4
Justin Vanderheide <justin@justinvanderheide.com>
2016-12-03 17:51:42 -0500

    Day 3: Squares With Three Sides

---
+++ 2016/03.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Advent03 {
+  public static void Main () {
+    string line;
+    int triangleCount = 0;
+
+    while ((line = Console.ReadLine()) != null) {
+      string[] rawSides = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+      int[] sides = rawSides.Select(int.Parse).OrderBy(side => side).ToArray();
+      if (sides[0] + sides[1] > sides[2]) {
+        triangleCount++;
+      }
+    }
+
+    Console.WriteLine("Number of triangles: " + triangleCount);
+  }
+}
---
+++ 2016/03_2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Advent032 {
+  public static void Main () {
+    string line = Console.ReadLine();
+    int triangleCount = 0;
+    int[][] triangles = new int[3][];
+
+    for (int i = 0; i < 3; i++) {
+      triangles[i] = new int[3];
+    }
+
+    while (line != null) {
+      for (int i = 0; i < 3; i++) {
+        string[] rawSides = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        for (int j = 0; j < 3; j++) {
+          triangles[j][i] = int.Parse(rawSides[j]);
+        }
+
+        line = Console.ReadLine();
+      }
+
+      for (int i = 0; i < 3; i++) {
+        Array.Sort(triangles[i]);
+        if (triangles[i][0] + triangles[i][1] > triangles[i][2]) {
+          triangleCount++;
+        }
+      }
+    }
+
+    Console.WriteLine("Number of triangles: " + triangleCount);
+  }
+}
