7bc49373b4b6eaa56fa2767806818eefe7815724
Justin Vanderheide <justin@justinvanderheide.com>
2016-12-09 00:21:51 -0500

    Day 9: Explosives in Cyberspace

---
+++ 2016/09.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Advent09 {
+  public static void Main () {
+    string result = "";
+    string line = Console.ReadLine();
+
+    for (int i = 0; i < line.Length; i++) {
+      if (line[i] == '(') {
+        int endOfExpansion = line.IndexOf(')', i);
+        string expansion = line.Substring(i + 1,  endOfExpansion - i - 1);
+        string[] lengthAndReps = expansion.Split(new char[] { 'x' });
+        int length = int.Parse(lengthAndReps[0]);
+        int reps = int.Parse(lengthAndReps[1]);
+
+        string substr = line.Substring(endOfExpansion + 1, length);
+        for (int j = 0; j < reps; j++) {
+          result += substr;
+        }
+        i = endOfExpansion + length;
+      } else {
+        result += line[i];
+      }
+    }
+
+    Console.WriteLine("Length: " + result.Length);
+  }
+}
---
+++ 2016/09_02.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Advent092 {
+  public static void Main () {
+    string line = Console.ReadLine();
+    Console.WriteLine("Length: " + GetLength(line));
+  }
+
+  private static long GetLength(string code) {
+    long length = 0;
+
+    for (int i = 0; i < code.Length; i++) {
+      if (code[i] == '(') {
+        int endOfExpansion = code.IndexOf(')', i);
+        string expansion = code.Substring(i + 1,  endOfExpansion - i - 1);
+        string[] lengthAndReps = expansion.Split(new char[] { 'x' });
+        int repLength = int.Parse(lengthAndReps[0]);
+        int reps = int.Parse(lengthAndReps[1]);
+        string substr = code.Substring(endOfExpansion + 1, repLength);
+
+        length += reps * GetLength(substr);
+        i = endOfExpansion + repLength;
+      } else {
+        length++;
+      }
+    }
+
+    return length;
+  }
+}
