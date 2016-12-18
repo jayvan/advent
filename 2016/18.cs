c7b0383f8098247c91bf802ec83dbfd0c9bb7082
Justin Vanderheide <justin@justinvanderheide.com>
2016-12-18 16:49:03 -0500

    Day 18: Like a Rogue

---
+++ 2016/18.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Advent18 {
+  public static void Main () {
+    const int NUM_ROWS = 40;
+
+    int safeCount = 0;
+    string initial = Console.ReadLine();
+
+    var currentFloor = new bool[initial.Length];
+
+    for (int i = 0; i < currentFloor.Length; i++) {
+      if (initial[i] == '^') {
+        currentFloor[i] = true;
+      } else {
+        safeCount++;
+      }
+    }
+
+    var floors = new bool[NUM_ROWS][];
+    floors[0] = currentFloor;
+
+    for (int i = 1; i < NUM_ROWS; i++) {
+      bool[] previous = floors[i - 1];
+      var newFloor = new bool[previous.Length];
+      floors[i] = newFloor;
+
+      for (int j = 0; j < newFloor.Length; j++) {
+        bool left = j > 0 && previous[j - 1];
+        bool center = previous[j];
+        bool right= j < newFloor.Length - 1 && previous[j + 1];
+
+        newFloor[j] = left && center && !right || !left && center && right || left && !center && !right || !left && !center && right;
+
+        if (!newFloor[j]) {
+          safeCount++;
+        }
+      }
+    }
+
+    Console.WriteLine($"Number of safe spots: {safeCount}");
+  }
+}
