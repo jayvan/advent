0b01f5f933ac0df9aa57dd6f10986a8dcf500ad6
Justin Vanderheide <justin@justinvanderheide.com>
2016-12-19 23:20:13 -0500

    Day 19: An Elephant Named Joseph

---
+++ 2016/19.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Advent19 {
+  public static void Main () {
+    int elfCount = int.Parse(Console.ReadLine());
+
+    var elves = new bool[elfCount];
+
+    int elfIndex = 0;
+
+    while (elfCount > 1) {
+      int nextElfIndex = (elfIndex + 1) % elves.Length;
+      nextElfIndex = Array.IndexOf(elves, false, nextElfIndex);
+
+      if (nextElfIndex == -1) {
+        nextElfIndex = Array.IndexOf(elves, false);
+      }
+
+      elves[nextElfIndex] = true;
+      elfCount--;
+
+      elfIndex = Array.IndexOf(elves, false, nextElfIndex);
+      if (elfIndex == -1) {
+        elfIndex = Array.IndexOf(elves, false);
+      }
+    }
+
+    Console.WriteLine("Last Elf standing: " + (elfIndex + 1));
+  }
+}
---
+++ 2016/19_2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Advent19 {
+  public static void Main () {
+    int elfCount = int.Parse(Console.ReadLine());
+
+    var elves = new LinkedList<int>();
+    for (int i = 1; i <= elfCount; i++) {
+      elves.AddLast(i);
+    }
+
+    LinkedListNode<int> currentNode = elves.First;
+    LinkedListNode<int> oppositeNode = elves.First;
+
+    for (int i = 0; i < elfCount / 2; i++) {
+      oppositeNode = oppositeNode.Next;
+    }
+
+    while (elfCount > 1) {
+      var nextNode = oppositeNode.Next;
+      if (nextNode == null) {
+        nextNode = elves.First;
+      }
+
+      elves.Remove(oppositeNode);
+
+      elfCount--;
+      if (elfCount % 2 == 0) {
+        nextNode = nextNode.Next;
+        if (nextNode == null) {
+          nextNode = elves.First;
+        }
+      }
+      oppositeNode = nextNode;
+
+      currentNode = currentNode.Next;
+      if (currentNode == null) {
+        currentNode = elves.First;
+      }
+    }
+
+    Console.WriteLine("Last Elf standing: " + (elves.First.Value));
+  }
+}
