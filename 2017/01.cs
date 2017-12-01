db7b3682c1a8beffeff2ae616f1309cdb402a67c
Justin Vanderheide <justin@justinvanderheide.com>
2017-12-01 08:21:16 -0500

    Day 1: Inverse Captcha

---
+++ 2017/01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Advent01 {
+  public static void Main () {
+    string numbers = Console.ReadLine();
+
+    Console.WriteLine("Part One: " + SumForOffset(numbers, 1));
+    Console.WriteLine("Part Two: " + SumForOffset(numbers, numbers.Length / 2));
+  }
+
+  private static double SumForOffset(string input, int offset) {
+    double sum = 0;
+
+    for (int i = 1; i < input.Length; i++) {
+      int otherIndex = (i + offset) % input.Length;
+
+      if (input[i] == input[otherIndex]) {
+        sum += Char.GetNumericValue(input[i]);
+      }
+    }
+
+    return sum;
+  }
+}
