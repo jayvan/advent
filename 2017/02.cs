0c68c6962a5001161edd14f74a59578e0f193ded
Justin Vanderheide <justin@justinvanderheide.com>
2017-12-02 17:03:06 -0500

    Day 2: Corruption Checksum

---
+++ 2017/02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Advent02 {
+  public static void Main () {
+    string line;
+    int checksum = 0;
+
+    while ((line = Console.ReadLine()) != null) {
+      int lowest = int.MaxValue;
+      int highest = int.MinValue;
+
+      foreach (string entry in line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)) {
+        int num = int.Parse(entry);
+        lowest = Math.Min(lowest, num);
+        highest = Math.Max(highest, num);
+      }
+
+      checksum += highest - lowest;
+    }
+
+    Console.WriteLine($"Checksum: {checksum}");
+  }
+}
---
+++ 2017/02_02.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+public class Advent02 {
+  public static void Main () {
+    string line;
+    int checksum = 0;
+
+    while ((line = Console.ReadLine()) != null) {
+      int[] entries = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToArray();
+      checksum += DivisiblePairResult(entries);
+    }
+
+    Console.WriteLine($"Checksum: {checksum}");
+  }
+
+  private static int DivisiblePairResult(int[] entries) {
+    Array.Sort(entries);
+    for (int i = 0; i < entries.Length; i++) {
+      for (int j = entries.Length - 1; j > i; j--) {
+        if (entries[i] > entries[j] / 2) {
+          break;
+        }
+
+        if (entries[j] % entries[i] == 0) {
+          return entries[j] / entries[i];
+        }
+      }
+    }
+
+    throw new ArgumentException("The given entries do not have two values where one divides the other");
+  }
+}
