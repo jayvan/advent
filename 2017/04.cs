bfd656fffaa9babf11ac6135639da2db002690d9
Justin Vanderheide <justin@justinvanderheide.com>
2017-12-04 08:22:32 -0500

    Day 4: High-Entropy Passphrases

---
+++ 2017/04.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Advent04 {
+
+  public static void Main () {
+    int validCount = 0;
+    string line;
+
+    while ((line = Console.ReadLine()) != null) {
+      string[] words = line.Split(' ');
+      var uniqueWords = new HashSet<string>(words);
+      if (uniqueWords.Count == words.Length) {
+        validCount++;
+      }
+    }
+
+    Console.WriteLine($"Valid Passwords: {validCount}");
+  }
+}
---
+++ 2017/04_02.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Advent04 {
+
+  public static void Main () {
+    int validCount = 0;
+    string line;
+
+    while ((line = Console.ReadLine()) != null) {
+      string[] words = line.Split(' ');
+      var uniqueWords = new HashSet<string>();
+
+      bool valid = true;
+      foreach (string word in words) {
+        char[] wordArray = word.ToCharArray();
+        Array.Sort(wordArray);
+        string sortedWord = new String(wordArray);
+
+        if (uniqueWords.Contains(sortedWord)) {
+          valid = false;
+          break;
+        }
+
+        uniqueWords.Add(sortedWord);
+      }
+
+
+      if (valid) {
+        validCount++;
+      }
+    }
+
+    Console.WriteLine($"Valid Passwords: {validCount}");
+  }
+}
