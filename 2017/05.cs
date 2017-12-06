049276e6cd811e8a243e72c12be64e8e98d585a8
Justin Vanderheide <justin@justinvanderheide.com>
2017-12-05 20:02:05 -0500

    Day 5: A Maze of Twisty Trampolines, All Alike

---
+++ 2017/05.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Advent05 {
+  public static void Main () {
+    int index = 0;
+    int jumps = 0;
+    var offsets = new List<int>();
+
+    string line;
+
+    while ((line = Console.ReadLine()) != null) {
+      offsets.Add(int.Parse(line));
+    }
+
+    while (index >= 0 && index < offsets.Count) {
+      index += offsets[index]++;
+      jumps++;
+    }
+
+    Console.WriteLine($"Jumps taken: {jumps}");
+  }
+}
---
+++ 2017/05_02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Advent05 {
+  public static void Main () {
+    int index = 0;
+    int jumps = 0;
+    var offsets = new List<int>();
+
+    string line;
+
+    while ((line = Console.ReadLine()) != null) {
+      offsets.Add(int.Parse(line));
+    }
+
+    while (index >= 0 && index < offsets.Count) {
+      int offset = offsets[index];
+
+      if (offset >= 3) {
+        offsets[index]--;
+      } else {
+        offsets[index]++;
+      }
+
+      index += offset;
+      jumps++;
+    }
+
+    Console.WriteLine($"Jumps taken: {jumps}");
+  }
+}
