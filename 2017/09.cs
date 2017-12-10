7d2f774d1e49d2e9d9663f5905071f1ba8590cd9
Justin Vanderheide <justin@justinvanderheide.com>
2017-12-10 18:47:00 -0500

    Day 9: Stream Processing

---
+++ 2017/09.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Advent09 {
+  public static void Main () {
+    string line;
+
+    while ((line = Console.ReadLine()) != null) {
+      ScoreStream(line);
+    }
+  }
+
+  private static void ScoreStream(string stream) {
+    int score = 0;
+    int groupDepth = 0;
+    int garbageLength = 0;
+    bool inGarbage = false;
+    // '{' represents the start of a *group*
+    // '}' represents the end of a *group*
+    // '<' enters *garbage* mode, where everything but a '>' is ignored
+    // The exception is that in garbage mode a '!' skips the next char
+
+    for (int i = 0; i < stream.Length; i++) {
+      if (inGarbage) {
+        if (stream[i] == '!') {
+          i++;
+        } else if (stream[i] == '>') {
+          inGarbage = false;
+        } else {
+          garbageLength++;
+        }
+      } else {
+        if (stream[i] == '<') {
+          inGarbage = true;
+        } else if (stream[i] == '{') {
+          groupDepth++;
+        } else if (stream[i] == '}') {
+          score += groupDepth--;
+        }
+      }
+    }
+
+    Console.WriteLine("Score was " + score);
+    Console.WriteLine("Garbage amount: " + garbageLength);
+  }
+}
