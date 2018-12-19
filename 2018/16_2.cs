16bcc3bb8569619378083aeff0894109523ab812
Justin Vanderheide <justin@justinvanderheide.com>
2018-12-18 20:11:41 -0500

    Day 17: Reservoir Research

---
+++ 2018/16_2.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Advent16_2 {
+  public static void Main() {
+    var commands = new Dictionary<string, Command>();
+    commands.Add("addr", new Command(true, true, (a, b) => a + b));
+    commands.Add("addi", new Command(true, false, (a, b) => a + b));
+    commands.Add("mulr", new Command(true, true, (a, b) => a * b));
+    commands.Add("muli", new Command(true, false, (a, b) => a * b));
+    commands.Add("banr", new Command(true, true, (a, b) => a & b));
+    commands.Add("bani", new Command(true, false, (a, b) => a & b));
+    commands.Add("borr", new Command(true, true, (a, b) => a | b));
+    commands.Add("bori", new Command(true, false, (a, b) => a | b));
+    commands.Add("setr", new Command(true, true, (a, b) => a));
+    commands.Add("seti", new Command(false, false, (a, b) => a));
+    commands.Add("gtir", new Command(false, true, (a, b) => a > b ? 1 : 0));
+    commands.Add("gtri", new Command(true, false, (a, b) => a > b ? 1 : 0));
+    commands.Add("gtrr", new Command(true, true, (a, b) => a > b ? 1 : 0));
+    commands.Add("eqir", new Command(false, true, (a, b) => a == b ? 1 : 0));
+    commands.Add("eqri", new Command(true, false, (a, b) => a == b ? 1 : 0));
+    commands.Add("eqrr", new Command(true, true, (a, b) => a == b ? 1 : 0));
+
+    string line;
+
+    while ((line = Console.ReadLine()) != string.Empty) {
+      int[] registers = line.Substring(9, line.Length - 10).Split(new[] {", "}, StringSplitOptions.None).Select(a => int.Parse(a)).ToArray();
+      int[] command = Console.ReadLine().Split(' ').Select(a => int.Parse(a)).ToArray();
+      int[] result = Console.ReadLine().Substring(9, line.Length - 10).Split(new[] {", "}, StringSplitOptions.None).Select(a => int.Parse(a)).ToArray();
+      Console.ReadLine();
+
+      foreach (var kvp in commands) {
+        int[] output = (int[]) registers.Clone();
+        kvp.Value.Run(command, output);
+
+        if (!output.SequenceEqual(result)) {
+          kvp.Value.RemoveOpCode(command[0]);
+        }
+      }
+    }
+
+    while (true) {
+      if (commands.Values.All(cmd => cmd.Determined)) {
+        break;
+      }
+
+      foreach (Command a in commands.Values) {
+        if (a.Determined) {
+          foreach (Command b in commands.Values) {
+            if (a != b) {
+              b.RemoveOpCode(a.OpCode);
+            }
+          }
+        }
+      }
+    }
+
+    var opCodes = new Command[16];
+    foreach (var cmd in commands.Values) {
+      opCodes[cmd.OpCode] = cmd;
+    }
+
+    Console.ReadLine();
+
+    int[] reg = new int[4];
+
+    while ((line = Console.ReadLine()) != null) {
+      int[] cmd = line.Split(' ').Select(a => int.Parse(a)).ToArray();
+      opCodes[cmd[0]].Run(cmd, reg);
+    }
+
+    Console.WriteLine(reg[0]);
+  }
+}
+
+public class Command {
+  private Func<int, int, int> operation;
+  private bool firstIsRegister;
+  private bool secondIsRegister;
+  public List<int> potentialOpCodes;
+  public bool Determined => this.potentialOpCodes.Count == 1;
+  public int OpCode => this.potentialOpCodes[0];
+
+  public Command(bool firstIsRegister, bool secondIsRegister, Func<int, int, int> operation) {
+    this.operation = operation;
+    this.firstIsRegister = firstIsRegister;
+    this.secondIsRegister = secondIsRegister;
+    this.potentialOpCodes = new List<int>();
+    for (int i = 0; i < 16; i++) {
+      this.potentialOpCodes.Add(i);
+    }
+  }
+
+  public void RemoveOpCode(int code) {
+    this.potentialOpCodes.Remove(code);
+  }
+
+  int FirstValue(int[] command, int[] registers) {
+    return this.firstIsRegister ? registers[command[1]] : command[1];
+  }
+
+  int SecondValue(int[] command, int[] registers) {
+    return this.secondIsRegister ? registers[command[2]] : command[2];
+  }
+
+  public void Run(int[] command, int[] registers) {
+    registers[command[3]] = this.operation(FirstValue(command, registers), SecondValue(command, registers));
+  }
+}
---
+++ 2018/17.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+public class Advent17 {
+  public static void Main() {
+    var sim = new WaterSim();
+
+    string line;
+
+    while ((line = Console.ReadLine()) != null) {
+      if (line[0] == 'x') {
+        int x = int.Parse(line.Split(',')[0].Split('=')[1]);
+        int[] ys = line.Split(' ')[1].Substring(2).Split(new[] {'.'}, StringSplitOptions.RemoveEmptyEntries).Select(a => int.Parse(a)).ToArray();
+
+        for (int y = ys[0]; y <= ys[1]; y++) {
+          sim.AddClay(x, y);
+        }
+      } else {
+        int y = int.Parse(line.Split(',')[0].Split('=')[1]);
+        int[] xs = line.Split(' ')[1].Substring(2).Split(new[] {'.'}, StringSplitOptions.RemoveEmptyEntries).Select(a => int.Parse(a)).ToArray();
+
+        for (int x = xs[0]; x <= xs[1]; x++) {
+          sim.AddClay(x, y);
+        }
+      }
+    }
+
+    sim.Simulate();
+    Console.WriteLine(sim.VisitCount);
+    Console.WriteLine(sim.WaterCount);
+  }
+
+  public class WaterSim {
+    private HashSet<Tuple<int, int>> Clay = new HashSet<Tuple<int, int>>();
+    private HashSet<Tuple<int, int>> Visited = new HashSet<Tuple<int, int>>();
+    private HashSet<Tuple<int, int>> Water = new HashSet<Tuple<int, int>>();
+    private int minY = int.MaxValue;
+    private int maxY = int.MinValue;
+    private int minX = int.MaxValue;
+    private int maxX = int.MinValue;
+
+    public int VisitCount => this.Visited.Count - 1;
+    public int WaterCount => this.Water.Count;
+
+    public void AddClay(int x, int y) {
+      Clay.Add(new Tuple<int, int>(x, y));
+      minY = Math.Min(minY, y);
+      maxY = Math.Max(maxY, y);
+      this.minX = Math.Min(this.minX, x - 1);
+      this.maxX = Math.Max(this.maxX, x + 1);
+    }
+
+    public void Simulate() {
+      HashSet<Tuple<int, int>> blocked = new HashSet<Tuple<int, int>>(Clay);
+      Stack<Tuple<int, int>> todo = new Stack<Tuple<int, int>>();
+      todo.Push(new Tuple<int, int>(500, this.minY - 1));
+
+      int iterations = 0;
+      while (todo.Count > 0) {
+        iterations++;
+        Tuple<int, int> point = todo.Pop();
+        if (iterations % 10000 == 0) {
+          Console.WriteLine($"Depth: {point.Item2}, Visited: {VisitCount}, Water: {WaterCount}");
+        }
+
+        Visited.Add(point);
+        // Go down as far as possible, enqueueing side checks along the way
+        int y;
+        bool hitFloor = false;
+        for (y = point.Item2 + 1; y <= this.maxY; y++) {
+          var location = new Tuple<int, int>(point.Item1, y);
+          if (blocked.Contains(location)) {
+            hitFloor = true;
+            y--;
+            break;
+          }
+          Visited.Add(location);
+        }
+
+        if (!hitFloor) {
+          continue;
+        }
+
+        int leftX = point.Item1;
+        bool leftWall = false;
+        while(leftX >= this.minX) {
+          leftWall = blocked.Contains(new Tuple<int, int>(leftX - 1, y));
+          bool hasFloor = blocked.Contains(new Tuple<int, int>(leftX, y + 1));
+
+          if (!hasFloor) {
+            todo.Push(new Tuple<int, int>(leftX, y));
+            break;
+          }
+
+          if (leftWall) {
+            break;
+          }
+          leftX--;
+        }
+
+        int rightX = point.Item1;
+        bool rightWall = false;
+        while (rightX <= this.maxX) {
+          rightWall = blocked.Contains(new Tuple<int, int>(rightX + 1, y));
+          bool hasFloor = blocked.Contains(new Tuple<int, int>(rightX, y + 1));
+
+          if (!hasFloor) {
+            todo.Push(new Tuple<int, int>(rightX, y));
+            break;
+          }
+
+          if (rightWall) {
+            break;
+          }
+
+          rightX++;
+        }
+
+        for (int x = leftX; x <= rightX; x++) {
+          var location = new Tuple<int, int>(x, y);
+          this.Visited.Add(location);
+
+          if (leftWall && rightWall) {
+            blocked.Add(location);
+            this.Water.Add(location);
+          }
+        }
+
+        if (leftWall && rightWall) {
+          todo.Push(new Tuple<int, int>(point.Item1, y - 1));
+        }
+      }
+    }
+
+    public void Draw() {
+      for (int y = this.minY; y <= this.maxY; y++) {
+        for (int x = this.minX; x <= this.maxX; x++) {
+          var location = new Tuple<int, int>(x, y);
+          if (this.Clay.Contains(location)) {
+            Console.Write('#');
+          } else if (this.Water.Contains(location)) {
+            Console.Write('~');
+          } else if (this.Visited.Contains(location)) {
+            Console.Write('|');
+          } else {
+            Console.Write('.');
+          }
+        }
+        Console.WriteLine();
+      }
+
+      Console.WriteLine("\n\n");
+    }
+  }
+}
