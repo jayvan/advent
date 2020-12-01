58027b3b81fa7a49f9ec54eb3781a315ae74ee38
Justin Vanderheide <justin@justinvanderheide.com>
2020-12-01 09:32:17 -0500

    Day 1: Report Repair

---
+++ 2020/01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Advent01 {
+  public static void Main () {
+    bool[] seen = new bool[2020];
+    string line;
+
+    while ((line = Console.ReadLine()) != null) {
+      int num = int.Parse(line);
+
+      if (num >= 2020) {
+        continue;
+      }
+
+      if (seen[2020 - num]) {
+        Console.WriteLine($"{num} + {2020 - num} = {num * (2020 - num)}");
+      }
+
+      seen[num] = true;
+    }
+  }
+}
---
+++ 2020/01_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Advent01 {
+  public static void Main () {
+    int[] numbers = new int[200];
+
+    for (int i = 0; i < 200; i++) {
+      numbers[i] = int.Parse(Console.ReadLine());
+    }
+
+    for (int a = 0; a < 200; a++) {
+      for (int b = a + 1; b < 200; b++) {
+        for (int c = b + 1; c < 200; c++) {
+          if (numbers[a] + numbers[b] + numbers[c] == 2020) {
+            Console.WriteLine(numbers[a] * numbers[b] * numbers[c]);
+          }
+        }
+      }
+    }
+  }
+}
