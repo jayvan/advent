250ef1a99c3d1b6048defc156d21a48521cbe5a7
Justin Vanderheide <justin@justinvanderheide.com>
2020-12-02 00:30:41 -0500

    Day 2: Password Philosophy

---
+++ 2020/02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Advent02 {
+  public static void Main () {
+    Regex regex = new Regex(@"([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)", RegexOptions.IgnoreCase);
+    string line;
+    int valid = 0;
+
+    while ((line = Console.ReadLine()) != null) {
+      Match match = regex.Match(line);
+
+      int min = int.Parse(match.Groups[1].Captures[0].Value);
+      int max = int.Parse(match.Groups[2].Captures[0].Value);
+      char c = match.Groups[3].Captures[0].Value[0];
+      int count = Array.FindAll(match.Groups[4].Captures[0].Value.ToCharArray(), a => a == c).Length;
+      if (min <= count && count <= max) {
+        valid++;
+      }
+    }
+      Console.WriteLine(valid);
+  }
+}
---
+++ 2020/02_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Advent022 {
+  public static void Main () {
+    Regex regex = new Regex(@"([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)", RegexOptions.IgnoreCase);
+    string line;
+    int valid = 0;
+
+    while ((line = Console.ReadLine()) != null) {
+      Match match = regex.Match(line);
+
+      int a = int.Parse(match.Groups[1].Captures[0].Value) - 1;
+      int b = int.Parse(match.Groups[2].Captures[0].Value) - 1;
+      char c = match.Groups[3].Captures[0].Value[0];
+      string password = match.Groups[4].Captures[0].Value;
+
+      if ((password[a] == c) ^ (password[b] == c)) {
+        valid++;
+      }
+    }
+      Console.WriteLine(valid);
+  }
+}
