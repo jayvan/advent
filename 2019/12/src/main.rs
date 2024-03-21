61b30f0b5083f20b6ea610475817356abd59b2a1
Justin Vanderheide <justin@justinvanderheide.com>
2024-03-21 08:48:29 -0400

    WIP

--- 2019/12/src/main.rs
+++ 2019/12/src/main.rs
@@ -1,15 +1,18 @@
-use std::cmp::Ordering;
+// Detect how long until 2 bodies will pass one another and simulate until they do, skipping time
+
 use std::collections::hash_map::DefaultHasher;
 use std::collections::{HashMap, HashSet};
 use std::hash::{Hash, Hasher};
 use std::io::stdin;
+use num;
 use std::time::SystemTime;
 use regex::Regex;
 
-#[derive(Hash,Clone)]
+#[derive(Hash,Clone,Debug)]
 struct Planet {
     position: [i32;3],
     velocity: [i32;3],
+    acceleration: [i32;3],
 }
 
 impl Planet {
@@ -17,31 +20,32 @@ impl Planet {
         Planet {
             position,
             velocity: [0;3],
+            acceleration: [0;3],
         }
     }
 
-    fn energy(&self) -> u32 {
-        let potential_energy = self.position[0].abs() + self.position[1].abs() + self.position[2].abs();
-        let kinetic_energy = self.velocity[0].abs() + self.velocity[1].abs() + self.velocity[2].abs();
-        (potential_energy * kinetic_energy) as u32
-    }
+    fn update_acceleration(&mut self, planets: &Vec<[i32;3]>) {
+        self.acceleration[0] = 0;
+        self.acceleration[1] = 0;
+        self.acceleration[2] = 0;
 
-    fn update_velocity(&mut self, planets: &Vec<[i32;3]>) {
         for planet in planets {
             for i in 0..3 {
-                match planet[i].cmp(&self.position[i]) {
-                    Ordering::Less => { self.velocity[i] -= 1 }
-                    Ordering::Equal => { }
-                    Ordering::Greater => { self.velocity[i] += 1}
-                }
+                self.acceleration[i] += num::clamp(planet[i] - self.position[i], -1, 1);
             }
         }
     }
 
+    fn update_velocity(&mut self) {
+        self.velocity[0] += self.acceleration[0];
+        self.velocity[1] += self.acceleration[1];
+        self.velocity[2] += self.acceleration[2];
+    }
+
     fn update_position(&mut self) {
-        for i in 0..self.position.len() {
-            self.position[i] += self.velocity[i];
-        }
+        self.position[0] += self.velocity[0];
+        self.position[1] += self.velocity[1];
+        self.position[2] += self.velocity[2];
     }
 }
 
@@ -62,13 +66,31 @@ fn main() {
 
     let mut iteration = 0;
     let mut time = SystemTime::now();
+
+
+    let mut previous_gravity: u64 = 0;
+    let mut previous_gravity_tick = iteration;
+
     loop {
         let positions:Vec<[i32;3]> = planets.iter().map(|planet| planet.position).collect();
-        planets.iter_mut().for_each(|p| p.update_velocity(&positions));
+        planets.iter_mut().for_each(|p| p.update_acceleration(&positions));
+        planets.iter_mut().for_each(|p| p.update_velocity());
         planets.iter_mut().for_each(|p| p.update_position());
 
         let mut hasher = DefaultHasher::new();
+        planets.iter().map(|planet| planet.acceleration).flatten().for_each(|g| hasher.write_i32(g));
+        let gravity_hash = hasher.finish();
+        if gravity_hash != previous_gravity {
+            println!("Gravity change on iteration {iteration}! After {}", iteration - previous_gravity_tick);
+            let gravity:Vec<[i32;3]> = planets.iter().map(|p| p.acceleration).collect();
+            println!("{:?}", gravity);
+            previous_gravity = gravity_hash;
+            previous_gravity_tick = iteration;
+        }
+
+        let mut hasher = DefaultHasher::new();
         planets.hash(&mut hasher);
+
         let is_new = seen.insert(hasher.finish());
 
         if !is_new {
