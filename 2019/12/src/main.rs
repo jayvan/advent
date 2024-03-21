e691f4e0ef9e5793c7142acc269294db3318f272
Justin Vanderheide <justin@justinvanderheide.com>
2024-03-21 14:34:54 -0400

    Day 12: The N-Body Problem

--- 2019/12/src/main.rs
+++ 2019/12/src/main.rs
@@ -1,108 +1,84 @@
-// Detect how long until 2 bodies will pass one another and simulate until they do, skipping time
-
-use std::collections::hash_map::DefaultHasher;
 use std::collections::{HashMap, HashSet};
-use std::hash::{Hash, Hasher};
+use std::error::Error;
+use std::hash::Hash;
 use std::io::stdin;
 use num;
-use std::time::SystemTime;
 use regex::Regex;
 
-#[derive(Hash,Clone,Debug)]
-struct Planet {
-    position: [i32;3],
-    velocity: [i32;3],
-    acceleration: [i32;3],
-}
+fn simulate_plane(original_positions: &[i16]) -> i32 {
+    let mut positions: [i16;4] = [original_positions[0], original_positions[1], original_positions[2], original_positions[3]];
+    let mut velocities:[i16;4] = [0;4];
+    let mut accelerations:[i16;4] = [0;4];
+    let mut iteration:i32 = 0;
+    let mut seen:HashSet<([i16;4], [i16;4])> = HashSet::new();
+    seen.insert((<[i16; 4]>::try_from(positions).unwrap(), velocities));
 
-impl Planet {
-    fn new(position: [i32;3]) -> Planet {
-        Planet {
-            position,
-            velocity: [0;3],
-            acceleration: [0;3],
-        }
-    }
+    loop {
+        accelerations.fill(0);
 
-    fn update_acceleration(&mut self, planets: &Vec<[i32;3]>) {
-        self.acceleration[0] = 0;
-        self.acceleration[1] = 0;
-        self.acceleration[2] = 0;
-
-        for planet in planets {
-            for i in 0..3 {
-                self.acceleration[i] += num::clamp(planet[i] - self.position[i], -1, 1);
+        // Calculate Accelerations
+        for planet in 0..4 {
+            for other in (planet + 1)..4 {
+                let delta = num::clamp(positions[other] - positions[planet], -1, 1);
+                accelerations[planet] += delta;
+                accelerations[other] -= delta;
             }
         }
-    }
 
-    fn update_velocity(&mut self) {
-        self.velocity[0] += self.acceleration[0];
-        self.velocity[1] += self.acceleration[1];
-        self.velocity[2] += self.acceleration[2];
-    }
+        // Update Velocity & position
+        for i in 0..4 {
+            velocities[i] += accelerations[i];
+            positions[i] += velocities[i];
+        }
 
-    fn update_position(&mut self) {
-        self.position[0] += self.velocity[0];
-        self.position[1] += self.velocity[1];
-        self.position[2] += self.velocity[2];
+        iteration += 1;
+        if !seen.insert((positions, velocities)) {
+            return iteration;
+        }
     }
 }
 
-fn main() {
+fn gcd(x: u64, y: u64) -> u64 {
+    let mut x = x;
+    let mut y = y;
+
+    while y != 0 {
+        let t = y;
+        y = x % y;
+        x = t;
+    }
+
+    x
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    (a * b) / gcd(a, b)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut buffer = String::new();
     let regex = Regex::new(r"<x=(-?\d+), y=(-?\d+), z=(-?\d+)>").expect("Invalid regex");
-    let mut planets:Vec<Planet> = Vec::new();
-    let mut seen:HashSet<u64> = HashSet::new();
 
-    while stdin().read_line(&mut buffer).is_ok_and(|size| size > 0) {
+    let mut positions:[i16;12] = [0;12];
+
+    for i in 0..4 {
+        stdin().read_line(&mut buffer)?;
+
         if let Some(captures) = regex.captures(&buffer) {
-            let position: [i32;3] = [captures[1].parse().unwrap(), captures[2].parse().unwrap(), captures[3].parse().unwrap()];
-            planets.push(Planet::new(position));
+            positions[i] = captures[1].parse().unwrap();
+            positions[4 + i] = captures[2].parse().unwrap();
+            positions[8 + i] = captures[3].parse().unwrap();
         }
 
         buffer.clear()
     }
 
-    let mut iteration = 0;
-    let mut time = SystemTime::now();
+    let x_phase = simulate_plane(&positions[0..4]) as u64;
+    let y_phase = simulate_plane(&positions[4..8]) as u64;
+    let z_phase = simulate_plane(&positions[8..12]) as u64;
+        ;
 
-
-    let mut previous_gravity: u64 = 0;
-    let mut previous_gravity_tick = iteration;
-
-    loop {
-        let positions:Vec<[i32;3]> = planets.iter().map(|planet| planet.position).collect();
-        planets.iter_mut().for_each(|p| p.update_acceleration(&positions));
-        planets.iter_mut().for_each(|p| p.update_velocity());
-        planets.iter_mut().for_each(|p| p.update_position());
-
-        let mut hasher = DefaultHasher::new();
-        planets.iter().map(|planet| planet.acceleration).flatten().for_each(|g| hasher.write_i32(g));
-        let gravity_hash = hasher.finish();
-        if gravity_hash != previous_gravity {
-            println!("Gravity change on iteration {iteration}! After {}", iteration - previous_gravity_tick);
-            let gravity:Vec<[i32;3]> = planets.iter().map(|p| p.acceleration).collect();
-            println!("{:?}", gravity);
-            previous_gravity = gravity_hash;
-            previous_gravity_tick = iteration;
-        }
-
-        let mut hasher = DefaultHasher::new();
-        planets.hash(&mut hasher);
-
-        let is_new = seen.insert(hasher.finish());
-
-        if !is_new {
-            println!("Finished after {iteration} steps");
-            break;
-        }
-
-        if iteration % 1000000 == 0 {
-            println!("{iteration}. {}", SystemTime::now().duration_since(time).unwrap().as_millis());
-            time = SystemTime::now();
-        }
-
-        iteration += 1;
-    }
+    let answer = lcm(lcm(x_phase, y_phase), z_phase);
+    println!("{x_phase}, {y_phase}, {z_phase} -> {answer}");
+    Ok(())
 }
